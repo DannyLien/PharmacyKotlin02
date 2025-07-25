3f01cad9624941239d46237a6a7c4e947c2dc509
DannyLien <danny.lien1109@gmail.com>
2025-07-26 02:23:12 +0800

    findViews, menu_exit;

--- app/src/main/java/com/hom/pharmacykotlin/MainActivity.kt
+++ app/src/main/java/com/hom/pharmacykotlin/MainActivity.kt
@@ -1,21 +1,68 @@
 package com.hom.pharmacykotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ProgressBar
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.hom.pharmacykotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+    private lateinit var spinnerCounty: Spinner
+    private lateinit var spinnerTown: Spinner
+    private lateinit var recy: RecyclerView
+    private val TAG: String? = MainActivity::class.java.simpleName
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate: mask- ")
+
+        findViews()
+    }
+
+    private fun findViews() {
+        spinnerCounty = binding.spinnerCounty
+        spinnerTown = binding.spinnerTown
+        recy = binding.recyclerPharm
+        progressBar = binding.progressBar
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuId = item.itemId
+        when(menuId){
+            R.id.action_exit->{
+                finish()
+                true
+            }
+
+            R.id.action_maps->{
+
+                true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
